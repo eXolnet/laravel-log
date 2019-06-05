747591ef3db7e3978e1452b7d3af1aebd6c9ba83
Patrick Gagnon-Renaud <pgrenaud@exolnet.com>
2019-06-05 16:29:13 -0400

    Add configurable log level with LOG_MONOLOG_LEVEL, default to APP_LOG_LEVEL

--- config/log.php
+++ config/log.php
@@ -3,6 +3,8 @@
 return [
     'app' => env('LOG_APP_NAME', env('APP_NAME')),
 
+    'level' => env('LOG_MONOLOG_LEVEL', env('APP_LOG_LEVEL', 'debug')),
+
     'host' => env('LOG_MONOLOG_HOST'),
 
     'port' => env('LOG_MONOLOG_PORT', 12201),
